cc056b814076bbaaee0dbd0f0a237457a61f400e
Gowthamoptit <104136996+Gowthamoptit@users.noreply.github.com>
2022-06-07 22:44:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-echo " This is task where install the LAMP on new EC2 instance"
+<html>
+  <html>
+     <title>Ansible Application</title>
+     </html>
+      <body>
+    <?php
+    echo " This is task where install the LAMP on new EC2 instance"
+    ?>
+
+     </body>
+     </html>
